806372e5f3b935934d9079224f4db233a7fa4042
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-07-31 17:33:32 +0600

    Up!

--- datasculpt.nim
+++ datasculpt.nim
@@ -1,4 +1,5 @@
-import typetraits
+import macros
+
 ## Data structures description language and modeling tool
 
 type
@@ -21,3 +22,10 @@ proc hasValue*[T](o: Option[T]): bool = o.hasValueF
 proc get*[T](o: Option[T]): T =
   assert hasValue(o)
   result = o.value
+
+iterator objFields*(T: typedesc): (NimNode, NimNode) =
+  ## Iterate object's fields
+  expectKind T.getType[1].getType, nnkObjectTy
+  let reclist = T.getType[1].getType[1]
+  for i in 0..len(reclist)-1:
+    yield (reclist[i], reclist[i].getType)
