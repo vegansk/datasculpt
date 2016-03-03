c237ecb5bfa92218c6a185da3f93771db8140aee
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-03-03 17:51:13 +0600

    One more dsl style

--- src/datasculpt/dsl.nim
+++ src/datasculpt/dsl.nim
@@ -2,15 +2,14 @@ import macros
 
 # This is an example of our DSL:
 dumpTree:
-  defType BaseObject(inheritable, immutable):
-    strField: string; notEmpty(); minLen(5); maxLen(100); desc("This is a string field")
+  defType BaseObject, inheritable, immutable, inherits BaseBaseObject:
+    strField string, notEmpty, minLen 5, maxLen 100, (dbField notNull, maxLen 200), desc "This is a string field"
     strAnotherField:
       string
-      dbField:
-        notNull
-        maxLen(200)
+      dbField: notNull; maxLen(200); linkedTo(AnotherObject.someField)
     optionField: Option[string]
         .desc("One more field")
         .notEmpty() 
         .minLen(5)
         .maxLen(100)
+
